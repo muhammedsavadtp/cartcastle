801dab708f9ea019ae922dd49bad910d0395777b
muhammed <www.savad34@gmail.com>
2023-03-30 09:14:53 +0530

    List Products

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,16 @@
 import Head from "next/head";
 import Layout from "../../components/Layout";
+import data from "../../utils/data";
+import ProductItem from "../../components/ProductItem";
 
 export default function Home() {
-  return <Layout title={'homePage'}>Home page</Layout>;
+  return <Layout title={'homePage'}>
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      {data.products.map((product,index) => {
+        return (
+          <ProductItem product={product} key={index}/>
+        )
+      })}
+    </div>
+  </Layout>;
 }
---
+++ utils/data.js
@@ -0,0 +1,71 @@
+const data = {
+  products: [
+    {
+      name: "t-Shirt",
+      stug: "fit—shirt",
+      category: "Shirts",
+      image:
+        "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/520899/01/mod01/fnd/IND/fmt/png/PUMA-Short-Sleeves-Men's-Training-T-Shirt",
+      price: 1199,
+      brand: "puma",
+      rating: 5,
+      numReviews: 20,
+      countlnStock: 5,
+      description: "A popular shirt • ",
+    },
+    {
+      name: " Short-Sleeve Top",
+      stug: "free—shirt",
+      category: "Shirts",
+      image:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/6905ffea-adf0-4696-b17d-8309085fe80d/sportswear-circa-french-terry-short-sleeve-top-Qzs4b9.png",
+      price: 1399,
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 7,
+      countlnStock: 25,
+      description: "A popular shirt • ",
+    },
+    {
+      name: "Free Shirt",
+      stug: "slim—shirt",
+      category: "Shirts",
+      image:
+        "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/22f3609fae8a4232bb2dafb600b34e1e_9366/Club_Tennis_Tee_Orange_HY4031_25_model.jpg",
+      price: 1299,
+      brand: "adidas",
+      rating: 4.7,
+      numReviews: 9,
+      countlnStock: 10,
+      description: "A popular shirt • ",
+    },
+    {
+      name: "nike pants",
+      stug: "golf—pant",
+      category: "pants",
+      image:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d164da21-1d3a-4fc1-875e-06288e806b15/jordan-flight-chicago-trousers-TJ3k7Q.png",
+      price: 2999,
+      brand: "nike",
+      rating: 4.7,
+      numReviews: 9,
+      countlnStock: 10,
+      description: "A popular shirt • ",
+    },
+    {
+      name: "adidas ",
+      stug: "slim—fit",
+      category: "pants",
+      image:
+        "https://assets.adidas.com/images/w_303,h_303,f_auto,q_auto,fl_lossy,c_fill,g_auto/696c63304a0a4fa8abbcaf5e00ece4a1_9366/adicolor-parley-track-pants.jpg",
+      price: 2799,
+      brand: "adidas",
+      rating: 4.7,
+      numReviews: 9,
+      countlnStock: 10,
+      description: "A popular shirt • ",
+    },
+  ],
+};
+
+export default data;
